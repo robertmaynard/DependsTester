940dc022a05bb743f69f074cccb0c0c571f14982
Robert Maynard <robert.maynard@kitware.com>
2012-01-17 13:32:10 -0500

    Updated the documentation.

--- Testing/test.bat
+++ Testing/test.bat
@@ -1,2 +1,2 @@
-call ..\DependsTester.py -p D:/Qt/qt-4.8.0x64/bin -d D:/Depends/depends.exe -a D:/Work/ParaView/build/bin/Debug/paraview.exe
+call ..\DependsTester.py --envpath D:/Qt/qt-4.8.0x64/bin D:/Depends/depends.exe D:/Work/ParaView/build/bin/Debug/paraview.exe --exit
 PAUSE 
