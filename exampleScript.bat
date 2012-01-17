36f76f96b40284bf0ae16613b02cd1541b960e34
Robert Maynard <robert.maynard@kitware.com>
2012-01-17 16:57:04 -0500

    Clarified what the bat file is for.

--- exampleScript.bat
+++
@@ -1,2 +0,0 @@
-call ..\DependsTester.py --envpath C:/Qt/bin C:/Depends/depends.exe C:/Work/ParaView/build/bin/Debug/paraview.exe --exit
-PAUSE 
---
+++ exampleUsage.bat
@@ -0,0 +1,2 @@
+call DependsTester.py --envpath C:/Qt/bin C:/Depends/depends.exe C:/Work/ParaView/build/bin/Debug/paraview.exe 
+PAUSE 
